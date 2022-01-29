e40762a87e59991573fd8ea0539a6a62b9934f8a
g33k5p34k <david.tanjx@gmail.com>
2022-01-28 18:30:38 -0700

    Several bugfixes involving required dependencies and the default sealvl file

--- R/getintervals.R
+++ R/getintervals.R
@@ -16,9 +16,6 @@ getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
   #time should be in kya (i.e. 10,000 years = 10 kya)
 
   message("Preparing interval file... ")
-  if (sealvl != bintanja_vandewal_2008) {
-    sealvl = read.csv(sealvl)
-  }
   time = as.numeric(time)
 
   #check time value to see if it makes sense
