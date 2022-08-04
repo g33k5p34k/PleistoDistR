b916fa792ec54fdad6a73c7249915664cc02bbc0
David <david.tanjx@gmail.com>
2022-08-05 02:06:54 +0800

    Major PleistoDist updates to comply with CRAN requirements. Also major bug updates for visibility calc and intervalfile generation

--- R/getintervals.R
+++ R/getintervals.R
@@ -5,21 +5,26 @@
 #'
 #' @param time Maximum time cutoff in kya. PleistoDist calculates the average distance between islands over a user-defined period of time. Users will have to specify an upper time bound (in thousands of years (kya)) for their PleistoDist analysis, which by default ranges from 0.1 kya to 3000 kya (i.e. 100 to 3,000,000 years ago). The lower time bound is fixed at the present day (0 kya).
 #' @param intervals Number of time interval bins to generate between the present day and the time-cutoff. More time intervals will lead to more fine-grained geomorphology calculations, at the expense of increased computational time. Under default conditions, the number of intervals should not exceed time-cutoff*10.
+#' @param outdir Output directory for the interval file. If the specified output directory doesn't already exist, PleistoDist will create the output directory.
 #' @param sealvl Optional parameter defining the sea level reconstruction file. By default, PleistoDist uses the sea level reconstruction from Bintanja & van de Wal (2008). If you use a custom sea level reconstruction file, make sure the file has a Time column (in kya) and a Sealevel_Corrected column (in metres), and is saved in CSV format.
 #' @return An interval file in the automatically-generated 'output' folder. This interval file is needed for all downstream PleistoDist analyses
 #' @examples
 #' \dontrun{
 #' #generate 20 time-binned intervals, with a cutoff of 20kya
-#' getintervals_time(time=20,intervals=20)
+#' getintervals_time(time=20,intervals=20,outdir="output")
 #' #if using a custom sea level reconstruction
 #' customsealvl <- read.csv("customsealvl.csv")
-#' getintervals_time(time=20,intervals=20,sealvl=customsealvl)
+#' getintervals_time(time=20,intervals=20,outdir="output",sealvl=customsealvl)
 #' }
 #' @export
-getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
+getintervals_time <- function(time,intervals,outdir,sealvl=bintanja_vandewal_2008) {
+
+  #create output folder
+  if (base::dir.exists(outdir)==FALSE) {
+    base::dir.create(outdir)
+  }
 
   #time should be in kya (i.e. 10,000 years = 10 kya)
-
   message("Preparing interval file... ")
   time = as.numeric(time)
 
@@ -30,14 +35,12 @@ getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
 
   intervals = as.numeric(intervals)
 
-  #error checking for interval value
-  if (intervals > time*10) {
-    stop("Number of intervals is too damn high!")
-  }
+  #calculate the time break between each row in the sea level reconstruction file
+  unittime = base::round(base::max(sealvl$Time)/base::length(sealvl$Time),digits=2)
 
-  #create outputfile
-  if (base::dir.exists("output")==FALSE) {
-    base::dir.create("output")
+  #error checking for interval value
+  if (intervals > base::nrow(dplyr::filter(sealvl, Time <= time))) {
+    stop("The number of intervals is too damn high!")
   }
 
   #subset sealvl table to only include rows within time cutoff
@@ -67,7 +70,7 @@ getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
     MeanDepth = 0,
     LowerTimeBound = 0,
     UpperTimeBound = 0,
-    TimeInterval = 0.1
+    TimeInterval = unittime
   )
 
   #this is the main for-loop that calculates the mean sea level for each time bin
@@ -77,17 +80,17 @@ getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
       dplyr::filter(Time <= timeinterval*x)
     intervalfile <- intervalfile %>% tibble::add_row(
       Interval = x,
-      MinDepth =  max(intvl$Sealevel_Corrected),
-      MaxDepth = min(intvl$Sealevel_Corrected),
+      MinDepth =  base::max(intvl$Sealevel_Corrected),
+      MaxDepth = base::min(intvl$Sealevel_Corrected),
       MeanDepth = base::mean(intvl$Sealevel_Corrected),
-      LowerTimeBound = min(intvl$Time),
-      UpperTimeBound = max(intvl$Time),
-      TimeInterval = length(intvl$Time)*0.1
+      LowerTimeBound = base::min(intvl$Time),
+      UpperTimeBound = base::max(intvl$Time),
+      TimeInterval = length(intvl$Time)*unittime
     )
   }
 
   #write output file
-  utils::write.csv(intervalfile,"output/intervals.csv")
+  utils::write.csv(intervalfile,base::paste0(outdir,"/intervals.csv"))
   message("Done!")
 }
 
@@ -97,6 +100,7 @@ getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
 #'
 #' @param time Maximum time cutoff in kya. PleistoDist calculates the average distance between islands over a user-defined period of time. Users will have to specify an upper time bound (in thousands of years (kya)) for their PleistoDist analysis, which by default can range from 0.1 kya to 3000 kya (i.e. 100 to 3,000,000 years ago). The lower time bound is fixed at the present day (0 kya).
 #' @param intervals Number of sea level interval bins to generate between the present day and the time-cutoff. More sea level intervals will lead to more fine-grained geomorphology calculations, at the expense of increased computational time.
+#' @param outdir Output directory for the interval file. If the specified output directory doesn't already exist, PleistoDist will create the output directory.
 #' @param sealvl Optional parameter defining the sea level reconstruction file. By default, PleistoDist uses the sea level reconstruction from Bintanja & van de Wal (2008). If you use a custom sea level reconstruction file, make sure the file has a Time column (in kya) and a Sealevel_Corrected column (in metres), and is saved in CSV format.
 #' @return An interval file in the automatically-generated 'output' folder. This interval file is needed for all downstream PleistoDist analyses
 #' @examples
@@ -108,7 +112,7 @@ getintervals_time <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
 #' getintervals_sealvl(time=20,intervals=20,sealvl=customsealvl)
 #' }
 #' @export
-getintervals_sealvl <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
+getintervals_sealvl <- function(time,intervals,outdir,sealvl=bintanja_vandewal_2008) {
 
   #time should be in kya (i.e. 10,000 years = 10 kya)
 
@@ -122,9 +126,12 @@ getintervals_sealvl <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
 
   intervals = as.numeric(intervals)
 
+  #calculate the time break between each row in the sea level reconstruction file
+  unittime = base::round(base::max(sealvl$Time)/base::length(sealvl$Time),digits=2)
+
   #create outputfile
-  if (base::dir.exists("output")==FALSE) {
-    base::dir.create("output")
+  if (base::dir.exists(outdir)==FALSE) {
+    base::dir.create(outdir)
   }
 
   #subset sealvl table to only include rows within time cutoff
@@ -153,7 +160,7 @@ getintervals_sealvl <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
     MinDepth = 0,
     MaxDepth = 0,
     MeanDepth = 0,
-    TimeInterval = 0.1
+    TimeInterval = unittime
   )
 
   for (x in 1:intervals) {
@@ -166,7 +173,7 @@ getintervals_sealvl <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
         MinDepth = max(intvl$Sealevel_Corrected),
         MaxDepth = min(intvl$Sealevel_Corrected),
         MeanDepth = (max(intvl$Sealevel_Corrected) + min(intvl$Sealevel_Corrected))/2,
-        TimeInterval = length(intvl$Sealevel_Corrected)*0.1
+        TimeInterval = length(intvl$Sealevel_Corrected)*unittime
       )
     }
     else {
@@ -175,13 +182,13 @@ getintervals_sealvl <- function(time,intervals,sealvl=bintanja_vandewal_2008) {
         dplyr::filter(Sealevel_Corrected >= depthinterval*x)
       intervalfile <- intervalfile %>% tibble::add_row(
         Interval = x,
-        MinDepth = max(intvl$Sealevel_Corrected),
-        MaxDepth = min(intvl$Sealevel_Corrected),
-        MeanDepth = (max(intvl$Sealevel_Corrected) + min(intvl$Sealevel_Corrected))/2,
-        TimeInterval = length(intvl$Sealevel_Corrected)*0.1
+        MinDepth = depthinterval*x,
+        MaxDepth = depthinterval*(x-1),
+        MeanDepth = (depthinterval*x + depthinterval*(x-1))/2,
+        TimeInterval = length(intvl$Sealevel_Corrected)*unittime
       )
     }
   }
-  utils::write.csv(intervalfile,"output/intervals.csv")
+  utils::write.csv(intervalfile,base::paste0(outdir,"/intervals.csv"))
   message("Done!")
 }
